1dcf9c0ad6d81b44815d7ee13fd10ea18435e944
zieluzieluzielu <mac.ziel@gmail.com>
2020-02-16 16:03:23 +0100

    Module 18

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -17,7 +17,7 @@ public class TaskController {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTASK")
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }

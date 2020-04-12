afcd7e23692673856d2920f948f19fc1c7d73a63
zieluzieluzielu <mac.ziel@gmail.com>
2020-04-12 20:33:40 +0200

    Module 22

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+}

8fa00ed2122ce43efb22131f0436fb303190bbf3
zieluzieluzielu <mac.ziel@gmail.com>
2020-05-16 17:05:22 +0200

    29.1 Facade pattern in project v2

---
+++ src/test/java/com/crud/tasks/facade/TrelloFacadeTest.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.facade;
+
+import com.crud.tasks.mapper.TrelloMapper;
+import com.crud.tasks.service.TrelloService;
+import com.crud.tasks.trello.facade.TrelloFacade;
+import com.crud.tasks.trello.validator.TrelloValidator;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrelloFacadeTest {
+
+    @InjectMocks
+    private TrelloFacade trelloFacade;
+
+    @Mock
+    private TrelloService trelloService;
+
+    @Mock
+    private TrelloValidator trelloValidator;
+
+    @Mock
+    private TrelloMapper trelloMapper;
+
+
+
+
+}
